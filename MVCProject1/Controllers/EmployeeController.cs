3b9737a0d0456c4211cafb56815bd81092624363
Abhinav Burra <64180259+abhinavburra@users.noreply.github.com>
2022-07-21 20:16:11 +0530

    Initial commit

---
+++ MVCProject1/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCProject1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee=new Employee()
+            {
+                EmployeeId = 1001,
+                EmployeeName = "Abhinav",
+                EmployeeLocation = "Hyderabad"
+            };
+            return View(employee);
+        }
+    }
+}
